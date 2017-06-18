5d478a86acb19b7e213894d32bf44864c14babe1
claresudbery <clareworking@claresudbery.co.uk>
2017-06-18 19:14:26 +0100

    Made it easier for generic novel tests to be common across all novels.

---
+++ TestDrivenNovel/GenericNovelTests.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using NovelDomain;
+using NovelDomain.ActualNovels.TheExperienceMaker;
+using NUnit.Framework;
+
+namespace TestDrivenNovel
+{
+    public static class GenericNovelTests 
+    {
+        public static void Protagonist_changes_emotion_during_all_chapters(INovel novel)
+        {
+            for (int chapter = 1; chapter <= novel.NumChapters(); chapter++)
+            {
+                // Arrange
+                string protagonistEmotionAtEndOfPreviousChapter = novel.GetProtagonistEmotionAtEndOfChapter(chapter - 1);
+
+                // Act
+                string protagonistEmotionAtEndOfChapterUnderTest = novel.GetProtagonistEmotionAtEndOfChapter(chapter);
+
+                // Assert
+                Assert.AreNotEqual(protagonistEmotionAtEndOfPreviousChapter, protagonistEmotionAtEndOfChapterUnderTest);
+            }
+        }
+
+        public static void Protagonist_emotion_at_end_of_each_chapter_is_same_as_emotion_at_start_of_next_chapter(INovel novel)
+        {
+            for (int chapter = 1; chapter <= novel.NumChapters(); chapter++)
+            {
+                // Arrange
+                string protagonistEmotionAtEndOfPreviousChapter = novel.GetProtagonistEmotionAtEndOfChapter(chapter - 1);
+
+                // Act
+                string protagonistEmotionAtStartOfChapterUnderTest = novel.GetProtagonistEmotionAtStartOfChapter(chapter);
+
+                // Assert
+                Assert.AreEqual(protagonistEmotionAtEndOfPreviousChapter, protagonistEmotionAtStartOfChapterUnderTest);
+            }
+        }
+
+        public static void Novel_starts_with_a_crisis(INovel novel)
+        {
+            // Act
+            Chapter chapter1 = novel.GetChapter(1);
+
+            // Assert
+            Assert.AreEqual(true, chapter1.GetEvent(1).IsCrisis);
+        }
+
+        public static void AllChaptersHaveTurningPoints(INovel novel)
+        {
+            for (int chapter = 1; chapter <= novel.NumChapters(); chapter++)
+            {
+                // Act
+                var turningPoint = novel.GetChapter(chapter).GetTurningPoint();
+
+                // Assert
+                Assert.IsNotNull(turningPoint);
+            }
+        }
+
+        public static void AllChaptersHaveOnlyOneTurningPoint(INovel novel)
+        {
+            for (int chapter = 1; chapter <= novel.NumChapters(); chapter++)
+            {
+                // Act
+                List<string> turningPoints = novel.GetChapter(chapter).GetAllTurningPoints();
+
+                // Assert
+                Assert.AreEqual(1, turningPoints.Count);
+            }
+        }
+    }
+}
--- TestDrivenNovel/StuckOnATrainTests/GenericNovelTests.cs
+++ TestDrivenNovel/StuckOnATrainTests/GenericNovelTests.cs
@@ -16,80 +16,31 @@ namespace TestDrivenNovel.StuckOnATrainTests
         [Test]
         public void Protagonist_changes_emotion_during_all_chapters()
         {
-            // Arrange
-            var novel = CreateNovel();
-
-            for (int chapter = 1; chapter <= novel.NumChapters(); chapter++)
-            {
-                string protagonistEmotionAtEndOfPreviousChapter = novel.GetProtagonistEmotionAtEndOfChapter(chapter - 1);
-
-                // Act
-                string protagonistEmotionAtEndOfChapterUnderTest = novel.GetProtagonistEmotionAtEndOfChapter(chapter);
-
-                // Assert
-                Assert.AreNotEqual(protagonistEmotionAtEndOfPreviousChapter, protagonistEmotionAtEndOfChapterUnderTest);
-            }
+            TestDrivenNovel.GenericNovelTests.Protagonist_changes_emotion_during_all_chapters(CreateNovel());
         }
 
         [Test]
         public void Protagonist_emotion_at_end_of_each_chapter_is_same_as_emotion_at_start_of_next_chapter()
         {
-            // Arrange
-            var novel = CreateNovel();
-
-            for (int chapter = 1; chapter <= novel.NumChapters(); chapter++)
-            {
-                string protagonistEmotionAtEndOfPreviousChapter = novel.GetProtagonistEmotionAtEndOfChapter(chapter - 1);
-
-                // Act
-                string protagonistEmotionAtStartOfChapterUnderTest = novel.GetProtagonistEmotionAtStartOfChapter(chapter);
-
-                // Assert
-                Assert.AreEqual(protagonistEmotionAtEndOfPreviousChapter, protagonistEmotionAtStartOfChapterUnderTest);
-            }
+            TestDrivenNovel.GenericNovelTests.Protagonist_emotion_at_end_of_each_chapter_is_same_as_emotion_at_start_of_next_chapter(CreateNovel());
         }
 
         [Test]
         public void Novel_starts_with_a_crisis()
         {
-            // Arrange
-            var novel = CreateNovel();
-
-            // Act
-            Chapter chapter1 = novel.GetChapter(1);
-
-            // Assert
-            Assert.AreEqual(true, chapter1.GetEvent(1).IsCrisis);
+            TestDrivenNovel.GenericNovelTests.Novel_starts_with_a_crisis(CreateNovel());
         }
 
         [Test]
         public void AllChaptersHaveTurningPoints()
         {
-            // Arrange
-            var novel = CreateNovel();
-
-            for (int chapter = 1; chapter <= novel.NumChapters(); chapter++)
-            {
-                var turningPoint = novel.GetChapter(chapter).GetTurningPoint();
-
-                // Assert
-                Assert.IsNotNull(turningPoint);
-            }
+            TestDrivenNovel.GenericNovelTests.AllChaptersHaveTurningPoints(CreateNovel());
         }
 
         [Test]
         public void AllChaptersHaveOnlyOneTurningPoint()
         {
-            // Arrange
-            var novel = CreateNovel();
-
-            for (int chapter = 1; chapter <= novel.NumChapters(); chapter++)
-            {
-                List<string> turningPoints = novel.GetChapter(chapter).GetAllTurningPoints();
-
-                // Assert
-                Assert.AreEqual(1, turningPoints.Count);
-            }
+            TestDrivenNovel.GenericNovelTests.AllChaptersHaveOnlyOneTurningPoint(CreateNovel());
         }
     }
 }
